9bdf2422c17f0e6efbc91e7f1230cdb1135603a7
xiongmao <1821655660@qq.com>
2025-03-20 02:06:27 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+import { codeInspectorPlugin } from 'code-inspector-plugin'; // 使用 import 语法替代 require
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => { // 移除未使用的参数 dev 和 isServer
+    config.plugins.push(codeInspectorPlugin({ bundler: 'webpack' }));
+    return config;
+  },
 };
 
 export default nextConfig;
